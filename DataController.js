e56723fea5e47f7cf78ce5e9b4612f98efef7728
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2021-02-05 17:31:44 +0600

    Added Logics

--- DataController.js
+++ DataController.js
@@ -1,5 +1,19 @@
 
 function validateEmail(email) {
+
+    if(email == null || email == "" || email == " "){
+
+        swal({
+            title: "Error Occured",
+            text: "Please enter an email. Field can't be empty!",
+            icon: "error",
+            button: "Try Again",
+          });
+
+        return;
+    }
+
+
     fetch(`https://zerobounce1.p.rapidapi.com/v2/validate?ip_address=replace_the_IP_address_the_email_signed_up_from&email=${email}`, {
         "method": "GET",
         "headers": {
@@ -9,18 +23,49 @@ function validateEmail(email) {
     })
         .then(res => res.json())
         .then(data => {
-            console.log(data);
-
-            let status = document.createElement('h5');
-            status.innerText = `Status: ${data.status}`;
-            document.getElementById("info-container").appendChild(status);
-
+        
             document.getElementById("status-panel").style.display = "block";
+            let container = document.getElementById("info-container")
+            document.getElementById("mail").innerText = data.address;
+
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+           
+
+           let status = document.createElement('h5');
+           let account = document.createElement('h5')
+           let domain = document.createElement('h5')
+           let freemail = document.createElement('h5')
+           let smtp = document.createElement('h5')
+           let domainAge = document.createElement('h5')
+
+            let freemailStatus;
+
+           status.innerText = `Status: ${data.status}`;
+           account.innerText = `Account: ${data.account}`;
+           domain.innerText = `Domain: ${data.domain}`;
+
+            if(data.freemail)
+                freemailStatus = "Yes";
+            else
+                freemailStatus = "No";    
+
+           freemail.innerText = `Is Free Mail: ${freemailStatus}`;
+           smtp.innerText = `SMTP Provider: ${data.smtp_provider}`;
+           domainAge.innerText = `Domain Age: ${data.domain_age_days} Days`;
+           
+           container.appendChild(status);
+           container.appendChild(account);
+           container.appendChild(domain);
+           container.appendChild(freemail);
+           container.appendChild(smtp);
+           container.appendChild(domainAge);
+
+        
         })
         .catch(err => {
             console.error(err);
         });
         
 }
-
-validateEmail('me.ashik.personal@gmail.com');
